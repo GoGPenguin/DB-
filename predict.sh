35d737dcc8b7acbe15f52cc14c05900c0852a3bb
Son_Nguyen <son_nguyentruong@datahouse.com>
2025-04-06 20:31:25 +0700

    update kaggle 4

--- predict.sh
+++ predict.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python tools/predict.py --model_path output/DBNet_resnet18_FPN_DBHead/checkpoint/model_best.pth --input_folder ./test/img --output_folder ./output --thre 0.7 --polygon --show --save_resut
+CUDA_VISIBLE_DEVICES=0 python tools/predict.py --model_path output/DBNet_resnet18_FPN_DBHead/checkpoint/model_best.pth --input_folder ./test --output_folder ./output --thre 0.7 --polygon --show --save_resut
