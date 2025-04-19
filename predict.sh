ee000a7386c149a9d47a6cf864da3a50431385f5
Son_Nguyen <son_nguyentruong@datahouse.com>
2025-04-19 18:39:43 +0700

    update

--- predict.sh
+++ predict.sh
@@ -1,1 +1,2 @@
 CUDA_VISIBLE_DEVICES=0 python tools/predict.py --model_path output/DBNet_resnet18_FPN_DBHead/checkpoint/model_best.pth --input_folder ./test --output_folder ./output --thre 0.7 --polygon --show --save_resut
+python DBNET.pytorch/tools/predict.py --model_path 3003/model_best.pth --input_folder ./DBNET.pytorch/test --output_folder ./output --thre 0.7 --polygon --show --save_resut
